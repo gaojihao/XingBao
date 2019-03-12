50f8a8883641145d470691f946947c1238fe539a
lizhi <lizhi1026@126.com>
2019-03-12 15:01:58 +0800

    add result file

--- src/main/java/com/lizhi/xingbao/model/CourseCategoty.java
+++ src/main/java/com/lizhi/xingbao/model/CourseCategoty.java
@@ -1,15 +1,30 @@
 package com.lizhi.xingbao.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "coursecategoty")
 public class CourseCategoty {
+
+    /**
+     * 功能描述:序列化时候的唯一性
+     */
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer categoryId;
 
     private String name;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
 
     public CourseCategoty(){
 
