7ddbc8634b3c8a1d3546c8e47b1d006ef577803d
lizhi <lizhi1026@126.com>
2019-03-16 00:00:47 +0800

    add VideoRespository

---
+++ src/main/java/com/lizhi/xingbao/respository/VideoRespository.java
@@ -0,0 +1,7 @@
+package com.lizhi.xingbao.respository;
+
+import com.lizhi.xingbao.model.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VideoRespository extends JpaRepository<Video,Integer> {
+}
