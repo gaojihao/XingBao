73acf9d84721a5e3392e8f806e673b0e7f5ce4e1
lizhi <lizhi1026@126.com>
2019-03-15 23:53:34 +0800

    add BlogRespository

---
+++ src/main/java/com/lizhi/xingbao/respository/BlogRespository.java
@@ -0,0 +1,8 @@
+package com.lizhi.xingbao.respository;
+
+import com.lizhi.xingbao.model.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BlogRespository extends JpaRepository<Blog,Integer>{
+}
