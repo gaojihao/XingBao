ab5ab67c7914e3e5daf208eed6f9ca90db846b07
lizhi <lizhi1026@126.com>
2019-03-19 20:34:17 +0800

    add table name for blog

--- src/main/java/com/lizhi/xingbao/model/Blog.java
+++ src/main/java/com/lizhi/xingbao/model/Blog.java
@@ -1,12 +1,10 @@
 package com.lizhi.xingbao.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
+@Table(name = "blog")
 public class Blog{
 
     private static final long serialVersionUID = 1L;
