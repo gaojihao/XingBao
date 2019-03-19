f26e03d3c25544345e2f4910a9394e49653d875f
lizhi <lizhi1026@126.com>
2019-03-19 21:02:18 +0800

    add BaseEntity

---
+++ src/main/java/com/lizhi/xingbao/model/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.lizhi.xingbao.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer Id;
+
+    @CreatedDate
+    private Long createTime;
+
+    @LastModifiedDate
+    private Long updateTime;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Long updateTime) {
+        this.updateTime = updateTime;
+    }
+}
--- src/main/java/com/lizhi/xingbao/model/Blog.java
+++ src/main/java/com/lizhi/xingbao/model/Blog.java
@@ -5,13 +5,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "blog")
-public class Blog{
-
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer blogId;
+public class Blog extends BaseEntity{
 
     public Blog(){
 
