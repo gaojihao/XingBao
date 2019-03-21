561663831297602fde89bbd436505662733fc63c
lizhi <lizhi1026@126.com>
2019-03-21 23:43:14 +0800

    add test

--- src/main/java/com/lizhi/xingbao/service/BlogService.java
+++ src/main/java/com/lizhi/xingbao/service/BlogService.java
@@ -1,5 +1,6 @@
 package com.lizhi.xingbao.service;
 
+import com.lizhi.xingbao.model.Blog;
 import com.lizhi.xingbao.respository.BlogRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class BlogService {
 
     @Autowired
     BlogRespository respository;
+
+    public Blog createBlog(String title,String content){
+        return null;
+    }
 }
