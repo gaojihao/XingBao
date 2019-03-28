cc297ea185b71ba0d1d6793fd2f51ca51ea9af73
lizhi <lizhi1026@126.com>
2019-03-28 15:09:16 +0800

    add user

--- src/main/java/com/lizhi/xingbao/model/User.java
+++ src/main/java/com/lizhi/xingbao/model/User.java
@@ -3,8 +3,94 @@ package com.lizhi.xingbao.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "xbuser")
 public class User extends BaseEntity{
+
+    private String phone;
+    private String password;
+
+    private String nickName;
+
+    private String jwt;
+
+    private String imgUrl;
+    private Boolean vip;
+    private String sex;
+    private String desc;
+    private short level;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getImgUrl() {
+        return imgurl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Boolean getVip() {
+        return vip;
+    }
+
+    public void setVip(Boolean vip) {
+        this.vip = vip;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public short getLevel() {
+        return level;
+    }
+
+    public void setLevel(short level) {
+        this.level = level;
+    }
+
 
     public User(){
 
