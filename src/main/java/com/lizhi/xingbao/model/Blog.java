a20ad0ee83b16b4c30a1599ad3de3dddd0bc17d0
lizhi <lizhi1026@126.com>
2019-03-29 14:27:41 +0800

    add Blog content

--- src/main/java/com/lizhi/xingbao/model/Blog.java
+++ src/main/java/com/lizhi/xingbao/model/Blog.java
@@ -11,8 +11,16 @@ import javax.persistence.*;
 
 public class Blog extends BaseEntity{
 
+    @Column(nullable = false,length = 50)
     private String title;
-    private String content;
+
+    @Column(nullable = false,length = 128)
+    private String summary;
+
+    @Column(nullable = false)
+    private String url;
+
+    private Integer readNum = 0;
 
 
     public String getTitle() {
@@ -23,11 +31,33 @@ public class Blog extends BaseEntity{
         this.title = title;
     }
 
-    public String getContent() {
-        return content;
+    public String getSummary() {
+        return summary;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getReadNum() {
+        return readNum;
+    }
+
+    public void setReadNum(Integer readNum) {
+        this.readNum = readNum;
+    }
+
+    public Blog(String title, String summary, String url) {
+        this.title = title;
+        this.summary = summary;
+        this.url = url;
     }
 }
