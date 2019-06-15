434c27ed81c2e60884ec2d70ca13bb96878550f3
lizhi <lizhi1026@126.com>
2019-06-15 23:49:26 +0800

    add field for star

--- src/main/java/com/lizhi/xingbao/model/Stared.java
+++ src/main/java/com/lizhi/xingbao/model/Stared.java
@@ -6,7 +6,37 @@ import javax.persistence.*;
 @Table(name = "star")
 public class Stared extends BaseEntity{
 
-    public Stared(){
+    /**
+     * 所属个人
+     */
+    private String userId;
 
+    /**
+     *所属课程
+     */
+    private Integer course;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Integer getCourse() {
+        return course;
+    }
+
+    public void setCourse(Integer course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Stared{" +
+                "userId='" + userId + '\'' +
+                ", course=" + course +
+                '}';
     }
 }
