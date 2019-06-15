f8cd40c6717a9a3b56d6ba69ac9b64ddc90a4b8f
lizhi <lizhi1026@126.com>
2019-06-15 23:50:49 +0800

    add field for tag

--- src/main/java/com/lizhi/xingbao/model/Tag.java
+++ src/main/java/com/lizhi/xingbao/model/Tag.java
@@ -6,7 +6,31 @@ import javax.persistence.*;
 @Table(name = "tag")
 public class Tag extends BaseEntity{
 
-    public Tag(){
+    private String name;
 
+    private String desc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
     }
 }
