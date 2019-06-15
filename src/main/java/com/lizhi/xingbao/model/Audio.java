2c2cde321a8a14c4101e3d2da8ffd6710cef0e0e
lizhi <lizhi1026@126.com>
2019-06-15 23:39:22 +0800

    add field for audio

--- src/main/java/com/lizhi/xingbao/model/Audio.java
+++ src/main/java/com/lizhi/xingbao/model/Audio.java
@@ -5,4 +5,66 @@ import javax.persistence.*;
 @Entity
 @Table(name = "audio")
 public class Audio extends BaseEntity{
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 资源地址
+     */
+    private String url;
+
+    /**
+     * 时长
+     */
+    private Integer duration;
+
+    /**
+     * 所属课程
+     */
+    private Integer course;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Integer getCourse() {
+        return course;
+    }
+
+    public void setCourse(Integer course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Audio{" +
+                "title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", duration=" + duration +
+                ", course=" + course +
+                '}';
+    }
 }
