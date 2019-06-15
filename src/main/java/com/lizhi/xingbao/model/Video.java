a077ee47e9714db0c8809630f720c23e24816897
lizhi <lizhi1026@126.com>
2019-06-15 23:51:47 +0800

    add field for video

--- src/main/java/com/lizhi/xingbao/model/Video.java
+++ src/main/java/com/lizhi/xingbao/model/Video.java
@@ -6,7 +6,65 @@ import javax.persistence.*;
 @Table(name = "video")
 public class Video extends BaseEntity{
 
-    public Video(){
+    /**
+     * 标题
+     */
+    private String title;
 
+    /**
+     * 资源地址
+     */
+    private String url;
+
+    /**
+     * 时长
+     */
+    private Integer duration;
+
+    /**
+     * 所属课程
+     */
+    private Integer course;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Integer getCourse() {
+        return course;
+    }
+
+    public void setCourse(Integer course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" +
+                "title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", duration=" + duration +
+                ", course=" + course +
+                '}';
     }
 }
