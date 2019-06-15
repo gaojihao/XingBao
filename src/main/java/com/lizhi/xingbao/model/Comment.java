35f510703965859f609172ff570877a914bd9ea5
lizhi <lizhi1026@126.com>
2019-06-15 23:47:59 +0800

    add field for comment

--- src/main/java/com/lizhi/xingbao/model/Comment.java
+++ src/main/java/com/lizhi/xingbao/model/Comment.java
@@ -6,5 +6,65 @@ import javax.persistence.*;
 @Table(name = "comment")
 public class Comment extends BaseEntity{
 
+    /**
+     * 所属个人
+     */
+    private String userId;
 
+    /**
+     *所属课程
+     */
+    private Integer course;
+
+    /**
+     * 评论内容
+     */
+    private String content;
+
+    /**
+     * 评分
+     */
+    private Integer level;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Integer getCourse() {
+        return course;
+    }
+
+    public void setCourse(Integer course) {
+        this.course = course;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "userId='" + userId + '\'' +
+                ", course=" + course +
+                ", content='" + content + '\'' +
+                ", level=" + level +
+                '}';
+    }
 }
