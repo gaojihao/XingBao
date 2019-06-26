b82f2291dff1c63d4914d897f420e15d6066ec8f
lizhi <lizhi1026@126.com>
2019-06-26 23:56:45 +0800

    add upload picture

--- src/main/java/com/lizhi/xingbao/controller/PictureController.java
+++ src/main/java/com/lizhi/xingbao/controller/PictureController.java
@@ -6,7 +6,9 @@ import com.lizhi.xingbao.service.PictureQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.print.Pageable;
 
@@ -21,5 +23,14 @@ public class PictureController extends BaseController {
     @GetMapping(value = "/pictures")
     public Result getPictures(Picture resources, Pageable pageable) {
         return Result.success(pictureQueryService.queryAll(resources, pageable));
+    }
+
+    /**
+     * 上传图片
+     * @param file
+     * @return
+     */
+    public Result upload(@RequestParam MultipartFile file) {
+        return  Result.success(pictureQueryService.upload(file));
     }
 }
