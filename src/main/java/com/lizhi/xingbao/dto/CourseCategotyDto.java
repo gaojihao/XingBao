723537f896c70283d29569c06e6188adf8fb6dd1
lizhi <lizhi1026@126.com>
2019-06-26 23:01:25 +0800

    add field for course category dto

--- src/main/java/com/lizhi/xingbao/dto/CourseCategotyDto.java
+++ src/main/java/com/lizhi/xingbao/dto/CourseCategotyDto.java
@@ -7,4 +7,19 @@ import java.io.Serializable;
 @Data
 public class CourseCategotyDto implements Serializable {
     private static final long serialVersionUID = 8282553267511034477L;
+
+    /**
+     * 标题
+     */
+    private String name;
+
+    /**
+     * 描述
+     */
+    private String desc;
+
+    /**
+     * 子标题
+     */
+    private String subTitle;
 }
