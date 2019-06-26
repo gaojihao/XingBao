28373a04e5fd4693dc7c818711a9964576e97511
lizhi <lizhi1026@126.com>
2019-06-26 22:59:25 +0800

    add field for course dto

--- src/main/java/com/lizhi/xingbao/dto/CommentDto.java
+++ src/main/java/com/lizhi/xingbao/dto/CommentDto.java
@@ -7,4 +7,24 @@ import java.io.Serializable;
 @Data
 public class CommentDto implements Serializable {
     private static final long serialVersionUID = -6619394898220969522L;
+
+    /**
+     * 所属个人
+     */
+    private String userId;
+
+    /**
+     *所属课程
+     */
+    private Integer course;
+
+    /**
+     * 评论内容
+     */
+    private String content;
+
+    /**
+     * 评分
+     */
+    private Integer level;
 }
--- src/main/java/com/lizhi/xingbao/dto/CourseDto.java
+++ src/main/java/com/lizhi/xingbao/dto/CourseDto.java
@@ -7,4 +7,64 @@ import java.io.Serializable;
 @Data
 public class CourseDto implements Serializable {
     private static final long serialVersionUID = 660510066789693598L;
+
+    /**
+     * 名称
+     */
+    private String title;
+
+    /**
+     *摘要
+     */
+    private String summary;
+
+    /**
+     * 描述
+     */
+    private String desc;
+
+    /**
+     * 子标题
+     */
+    private String subTitle;
+
+    /**
+     * 封面图标
+     */
+    private String coverImage;
+
+    /**
+     * 所属分类ID
+     */
+    private Integer category;
+
+    /**
+     * 所属分类名称
+     */
+    private String categoryName;
+
+    /**
+     * 实际价格
+     */
+    private Integer realPrice;
+
+    /**
+     * 市场价格
+     */
+    private Integer price;
+
+    /**
+     * 购买人数
+     */
+    private Integer buyCount;
+
+    /**
+     * 评分
+     */
+    private float grade;
+
+    /**
+     * 类型
+     */
+    private Integer type;
 }
