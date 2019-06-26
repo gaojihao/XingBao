49856ff8182240bd243741e9755576df779a1be0
lizhi <lizhi1026@126.com>
2019-06-27 00:01:51 +0800

    update

--- src/main/java/com/lizhi/xingbao/controller/PictureController.java
+++ src/main/java/com/lizhi/xingbao/controller/PictureController.java
@@ -13,10 +13,15 @@ import java.awt.print.Pageable;
 @RequestMapping("/api")
 public class PictureController extends BaseController {
 
-
     @Autowired
     private PictureQueryService pictureQueryService;
 
+    /**
+     * 查询图片
+     * @param resources
+     * @param pageable
+     * @return
+     */
     @GetMapping(value = "/pictures")
     public Result getPictures(Picture resources, Pageable pageable) {
         return Result.success(pictureQueryService.queryAll(resources, pageable));
@@ -33,6 +38,11 @@ public class PictureController extends BaseController {
     }
 
 
+    /**
+     * 删除图片
+     * @param id
+     * @return
+     */
     @GetMapping(value = "/pictures/{id}")
     public Result delete(@PathVariable Integer id){
         return Result.success(null);
