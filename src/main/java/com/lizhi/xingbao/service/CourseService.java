fbfb6474c3bfb0d4042573149de47b2b66e75bc8
lizhi <lizhi1026@126.com>
2019-07-02 23:52:30 +0800

    add CourseService biz

--- src/main/java/com/lizhi/xingbao/service/CourseService.java
+++ src/main/java/com/lizhi/xingbao/service/CourseService.java
@@ -1,6 +1,19 @@
 package com.lizhi.xingbao.service;
 
 
+import com.lizhi.xingbao.model.Course;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
 public interface CourseService {
+
+    List<Course> queryAll(Course course, Pageable pageable);
+
+    Course findOne(Integer id);
+
+    Course add(Course course);
+
+
 
 }
