79226a8abe826b3544a56512faf70685f547744f
lizhi <lizhi1026@126.com>
2019-07-10 23:08:57 +0800

    add mapper

---
+++ src/main/java/com/lizhi/xingbao/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.lizhi.xingbao.mapper;
+
+import com.lizhi.xingbao.dto.AccountDto;
+import com.lizhi.xingbao.entity.Account;
+import com.lizhi.xingbao.utils.EntityMapper;
+
+public interface UserMapper extends EntityMapper<AccountDto, Account> {
+}
