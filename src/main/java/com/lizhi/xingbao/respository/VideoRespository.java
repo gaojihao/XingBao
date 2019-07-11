67cd63058abcecc20054e2d606012ae15996464f
lizhi <lizhi1026@126.com>
2019-07-11 23:28:01 +0800

    query all by course

--- src/main/java/com/lizhi/xingbao/respository/VideoRespository.java
+++ src/main/java/com/lizhi/xingbao/respository/VideoRespository.java
@@ -1,9 +1,14 @@
 package com.lizhi.xingbao.respository;
 
 import com.lizhi.xingbao.entity.Video;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface VideoRespository extends JpaRepository<Video,Integer> {
 
     Video findByTitle(String title);
+
+    List<Video> queryAllByCourse(Integer course);
 }
