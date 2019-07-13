1f0c555edb520024608fef78cdbc3eb96c775394
lizhi <lizhi1026@126.com>
2019-07-13 22:18:45 +0800

    add blog action

--- src/main/java/com/lizhi/xingbao/controller/BlogController.java
+++ src/main/java/com/lizhi/xingbao/controller/BlogController.java
@@ -1,10 +1,15 @@
 package com.lizhi.xingbao.controller;
 
 import com.lizhi.xingbao.common.Result;
+import com.lizhi.xingbao.dto.BlogDto;
 import com.lizhi.xingbao.respository.BlogRespository;
+import com.lizhi.xingbao.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Pageable;
 
 
 @RestController
@@ -12,11 +17,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class BlogController extends BaseController{
 
     @Autowired
-    private BlogRespository dao;
+    private BlogService blogService;
 
     @RequestMapping("/create")
-    public Result createBlog(String title, String content){
+    public void createBlog(@RequestBody BlogDto blogDto){
 
-        return Result.fail("error");
+        Result.success(null);
     }
+
+    @RequestMapping("/delete")
+    public void delete(@RequestBody Integer Id){
+
+        Result.success(null);
+    }
+
+    @RequestMapping("/edit")
+    public void edit(@RequestBody BlogDto blogDto){
+
+        Result.success(null);
+    }
+
+    @RequestMapping("/detail")
+    public Result detail(@RequestBody Integer Id){
+
+        return Result.success(null);
+    }
+
+    @RequestMapping("/list")
+    public Result list( BlogDto blogDto,@RequestBody Pageable pageable){
+
+        return Result.success(null);
+    }
+
+
+
 }
