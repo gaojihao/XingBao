45a46d433cf91e7aa4d4fb3e8140b36bcfc4e409
lizhi <lizhi1026@126.com>
2019-07-13 22:08:08 +0800

    add audio action

--- src/main/java/com/lizhi/xingbao/controller/AudioController.java
+++ src/main/java/com/lizhi/xingbao/controller/AudioController.java
@@ -1,11 +1,15 @@
 package com.lizhi.xingbao.controller;
 
 import com.lizhi.xingbao.common.Result;
+import com.lizhi.xingbao.entity.Audio;
 import com.lizhi.xingbao.respository.AudioRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Pageable;
 
 @RestController
 @RequestMapping("xingbao/api/v1/audio")
@@ -15,7 +19,29 @@ public class AudioController extends BaseController{
     AudioRespository respository;
 
     @PostMapping("/add")
-    public Result add() {
+    public void add(@RequestBody Audio audio) {
+        Result.success(null);
+    }
+
+    @PostMapping("/edit")
+    public Result edit(@RequestBody Audio audio){
+        Result.success(null);
+    }
+
+    @PostMapping("/detail")
+    public Result detail(@RequestBody Integer id){
         return Result.success(null);
     }
+
+    @PostMapping("/delete")
+    public void delete(@RequestBody Integer id){
+        Result.success(null);
+    }
+
+    @PostMapping("/list")
+    public Result query( Audio audio, @RequestBody Pageable pageable) {
+        return Result.success(null);
+    }
+
+
 }
