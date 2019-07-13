e4eb0d70f6c2cdaf52a864710173808302c761f4
lizhi <lizhi1026@126.com>
2019-07-13 22:56:22 +0800

    collect manager

--- src/main/java/com/lizhi/xingbao/controller/CollectController.java
+++ src/main/java/com/lizhi/xingbao/controller/CollectController.java
@@ -1,7 +1,11 @@
 package com.lizhi.xingbao.controller;
 
+import com.lizhi.xingbao.common.Result;
+import com.lizhi.xingbao.request.CollectRequest;
 import com.lizhi.xingbao.service.CollectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,19 @@ public class CollectController extends BaseController{
     @Autowired
     private CollectService collectService;
 
+    @PostMapping("/add")
+    public void add(@RequestBody CollectRequest request) {
+        Result.success(null);
+    }
+
+    @PostMapping("/detail")
+    public Result detail(@RequestBody CollectRequest request) {
+        return Result.success(null);
+    }
+
+    @PostMapping("/list")
+    public Result list(@RequestBody CollectRequest request) {
+        return Result.success(null);
+    }
 
 }
---
+++ src/main/java/com/lizhi/xingbao/request/CollectRequest.java
@@ -0,0 +1,7 @@
+package com.lizhi.xingbao.request;
+
+public class CollectRequest {
+    private Integer courseId;
+    private Integer userId;
+    private Boolean isAdd;
+}
