45cc6917617daebe312b4bb1f81da86a7099cd14
lizhi <lizhi1026@126.com>
2019-07-13 22:24:30 +0800

    add category interface

--- src/main/java/com/lizhi/xingbao/controller/CategoryController.java
+++ src/main/java/com/lizhi/xingbao/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package com.lizhi.xingbao.controller;
 
 import com.lizhi.xingbao.common.Result;
-import com.lizhi.xingbao.respository.CourseCategoryRespository;
+import com.lizhi.xingbao.dto.CourseCategotyDto;
+import com.lizhi.xingbao.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("xingbao/api/v1/category")
 public class CategoryController extends BaseController{
     @Autowired
-    private CourseCategoryRespository respository;
+    private CourseService courseService;
 
-    @RequestMapping("saveCategory")
-    public Result saveCategory(String name){
-
-        return Result.fail("111");
+    @RequestMapping("add")
+    public void add(@RequestBody CourseCategotyDto dto){
+        Result.success(null);
     }
+
+    @RequestMapping("delete")
+    public void delete(@RequestBody Integer Id){
+        Result.success(null);
+    }
+
+    @RequestMapping("edit")
+    public Result edit(@RequestBody CourseCategotyDto dto){
+        return Result.success(null);
+    }
+
+    @RequestMapping("detail")
+    public Result detail(@RequestBody Integer Id){
+        return Result.success(null);
+    }
+
+    @RequestMapping("list")
+    public Result list(){
+        return Result.success(null);
+    }
+
 }
