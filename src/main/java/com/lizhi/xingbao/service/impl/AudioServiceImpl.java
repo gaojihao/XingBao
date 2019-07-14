91b44c58f6fb69709b7ce3bf1b30e8fa9d0c5a21
lizhi <lizhi1026@126.com>
2019-07-14 22:54:22 +0800

    add audio service method

--- src/main/java/com/lizhi/xingbao/service/impl/AudioServiceImpl.java
+++ src/main/java/com/lizhi/xingbao/service/impl/AudioServiceImpl.java
@@ -1,11 +1,14 @@
 package com.lizhi.xingbao.service.impl;
 
+import com.lizhi.xingbao.dto.AudioDto;
 import com.lizhi.xingbao.respository.AudioRespository;
 import com.lizhi.xingbao.service.AudioService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("AudioService")
 public class AudioServiceImpl implements AudioService {
@@ -14,5 +17,31 @@ public class AudioServiceImpl implements AudioService {
     @Autowired
     private AudioRespository respository;
 
+    @Override
+    public void createAudio(AudioDto dto){
 
+    }
+
+    @Override
+    public void editAudio(AudioDto dto){
+
+    }
+
+
+    @Override
+    public AudioDto audioDetial(Integer Id){
+        return null;
+    }
+
+
+    @Override
+    public void deleteAudio(Integer Id) {
+
+    }
+
+
+    @Override
+    public List<AudioDto> getAudioList(Integer courseId){
+        return null;
+    }
 }
