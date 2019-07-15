230e93fc223ef314bf68f478af429bb19324fc01
lizhi <lizhi1026@126.com>
2019-07-15 23:39:37 +0800

    update

--- src/main/java/com/lizhi/xingbao/common/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/lizhi/xingbao/common/Exception/GlobalExceptionHandler.java
@@ -3,12 +3,9 @@ package com.lizhi.xingbao.common.Exception;
 import com.lizhi.xingbao.common.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,8 +16,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Result> handleUnhandledException() {
         return ResponseEntity.ok(Result.UNKOWN_ERROR);
     }
