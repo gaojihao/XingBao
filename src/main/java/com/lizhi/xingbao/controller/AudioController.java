6ae1365a7121a0cd6adacdb9a244b142827645fb
lizhi <lizhi1026@126.com>
2019-07-16 22:14:56 +0800

    add control imp

--- src/main/java/com/lizhi/xingbao/controller/AudioController.java
+++ src/main/java/com/lizhi/xingbao/controller/AudioController.java
@@ -18,27 +18,32 @@ public class AudioController extends BaseController{
 
     @PostMapping("/add")
     public void add(@RequestBody AudioDto audio) {
+        service.createAudio(audio);
         Result.success(null);
     }
 
     @PostMapping("/edit")
     public void edit(@RequestBody AudioDto dto){
+        service.editAudio(dto);
         Result.success(null);
     }
 
     @PostMapping("/detail")
     public Result detail(@RequestBody Integer id){
-        return Result.success(null);
+
+        return Result.success(service.audioDetail(id));
     }
 
     @PostMapping("/delete")
     public void delete(@RequestBody Integer id){
+        service.deleteAudio(id);
         Result.success(null);
     }
 
     @PostMapping("/list")
     public Result query(@RequestBody Integer course) {
-        return Result.success(null);
+
+        return Result.success(service.getAudioList(course));
     }
 
 }
