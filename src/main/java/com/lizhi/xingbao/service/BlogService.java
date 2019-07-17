8a12b97d35d19fbd403354628f08ca3001b0f3a7
lizhi <lizhi1026@126.com>
2019-07-17 23:55:38 +0800

    add interface

--- src/main/java/com/lizhi/xingbao/service/BlogService.java
+++ src/main/java/com/lizhi/xingbao/service/BlogService.java
@@ -1,5 +1,20 @@
 package com.lizhi.xingbao.service;
 
-public interface BlogService {
+import com.lizhi.xingbao.dto.BlogDto;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
+public interface BlogService {
+    void createBlog(BlogDto dto);
+
+    void editBlog(BlogDto dto);
+
+    BlogDto blogDetail(Integer Id);
+
+    void deleteBlog(Integer Id);
+
+    List<BlogDto> getAllBlog();
+
+    List<BlogDto> queryBlog(Pageable pageable);
 }
