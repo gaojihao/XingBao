6766dec98afea2441f91683c03996d608d294901
lizhi <lizhi1026@126.com>
2019-07-24 23:38:14 +0800

    impl blog method

--- src/main/java/com/lizhi/xingbao/service/impl/BlogServiceImpl.java
+++ src/main/java/com/lizhi/xingbao/service/impl/BlogServiceImpl.java
@@ -1,11 +1,15 @@
 package com.lizhi.xingbao.service.impl;
 
+import com.lizhi.xingbao.dto.BlogDto;
 import com.lizhi.xingbao.respository.BlogRespository;
 import com.lizhi.xingbao.service.BlogService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("BlogService")
 public class BlogServiceImpl implements BlogService {
@@ -13,4 +17,34 @@ public class BlogServiceImpl implements BlogService {
 
     @Autowired
     private BlogRespository blogRespository;
+
+    @Override
+    public void createBlog(BlogDto dto) {
+
+    }
+
+    @Override
+    public void editBlog(BlogDto dto){
+
+    }
+
+    @Override
+    public BlogDto blogDetail(Integer Id){
+
+    }
+
+    @Override
+    public void deleteBlog(Integer Id){
+
+    }
+
+    @Override
+    public List<BlogDto> getAllBlog(){
+
+    }
+
+    @Override
+    public List<BlogDto> queryBlog(Pageable pageable){
+
+    }
 }
