99e7c4020714c69b24295ac9506426c255f96dc5
lizhi <lizhi1026@126.com>
2019-07-28 00:44:45 +0800

    add

--- src/main/java/com/lizhi/xingbao/controller/CommentController.java
+++ src/main/java/com/lizhi/xingbao/controller/CommentController.java
@@ -1,14 +1,14 @@
 package com.lizhi.xingbao.controller;
 
 import com.lizhi.xingbao.common.Result;
+import com.lizhi.xingbao.dto.CommentDto;
 import com.lizhi.xingbao.request.CommentRequest;
 import com.lizhi.xingbao.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.print.Pageable;
 
 @RestController
 @RequestMapping("xingbao/api/v1/comment")
@@ -18,24 +18,20 @@ public class CommentController extends BaseController{
     private CommentService commentService;
 
     @RequestMapping("add")
-    public void add(@RequestBody CommentRequest request) {
+    public void add(@RequestBody CommentDto dto) {
+        commentService.createComment(dto);
         Result.success(null);
     }
 
     @RequestMapping("delete")
     public void delete(@RequestBody Integer Id) {
+        commentService.deleteDetail(Id);
         Result.success(null);
     }
 
     @RequestMapping("list")
-    public Result list(CommentRequest request, @RequestBody Pageable pageable){
-        return Result.success(null);
+    public Result list(@RequestBody Integer courseId, @RequestBody Pageable pageable){
+
+        return Result.success(commentService.findAllBy(courseId,pageable));
     }
-
-    @RequestMapping("course/comment")
-    public Result courseComment(@RequestBody CommentRequest request){
-        return Result.success(null);
-    }
-
-
 }
