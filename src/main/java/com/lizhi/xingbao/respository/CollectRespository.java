4f831f115e6189992e25f63feba2d5e85989ac63
lizhi <lizhi1026@126.com>
2019-07-27 23:29:40 +0800

    add

--- src/main/java/com/lizhi/xingbao/respository/CollectRespository.java
+++ src/main/java/com/lizhi/xingbao/respository/CollectRespository.java
@@ -1,7 +1,11 @@
 package com.lizhi.xingbao.respository;
 
 import com.lizhi.xingbao.entity.Collect;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CollectRespository extends JpaRepository<Collect,Integer> {
+    List<Collect> findAllByUserId(Integer userId, Pageable pageable);
 }
