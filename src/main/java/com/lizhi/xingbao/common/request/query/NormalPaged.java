ac59741cb964ca44ce4b87467f65710cc48f2f7f
lizhi <lizhi1026@126.com>
2019-09-12 23:30:53 +0800

    add paged request

---
+++ src/main/java/com/lizhi/xingbao/common/request/query/NormalPaged.java
@@ -0,0 +1,12 @@
+package com.lizhi.xingbao.common.request.query;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+public class NormalPaged extends BasePaged {
+
+}
