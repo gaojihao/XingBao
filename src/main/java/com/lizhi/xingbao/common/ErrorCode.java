87b098d31b4bab0039fc47bda2915c66f81e3edd
lizhi <lizhi1026@126.com>
2019-09-12 22:12:59 +0800

    update ErrorCode

---
+++ src/main/java/com/lizhi/xingbao/common/ErrorCode.java
@@ -0,0 +1,8 @@
+package com.lizhi.xingbao.common;
+
+public interface ErrorCode {
+
+    String SUCCESS_CODE = "200";
+    String INTERNAL_CODE = "500";
+    String ILLEGAL_PARAMETER_CODE = "412";
+}
