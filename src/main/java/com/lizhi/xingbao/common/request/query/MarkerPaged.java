57eb46731935159bdc5365883d19dbf33a281f38
lizhi <lizhi1026@126.com>
2019-09-13 22:21:33 +0800

    add MarkerPaged biz

--- src/main/java/com/lizhi/xingbao/common/request/query/MarkerPaged.java
+++ src/main/java/com/lizhi/xingbao/common/request/query/MarkerPaged.java
@@ -10,4 +10,33 @@ import lombok.experimental.Accessors;
 public class MarkerPaged extends BasePaged {
 
     private static final long serialVersionUID = -4637211484393863203L;
+
+    /**
+     * 分页查询开始标识
+     */
+    private String marker;
+
+    /**
+     * 是否向前查询
+     * forward = true,marker=null 表示查第一页
+     * forward = true,marker="xxxxxx" 表示查以xxxxxx开始的下一页
+     * forward = false,marker=null 表示查最后一页
+     * forward = false,marker="xxxxxx" 表示查以xxxxxx结束的上一页
+     */
+    private boolean forward;
+
+    public MarkerPaged() {
+
+    }
+
+    public MarkerPaged(String marker, boolean forward, int itemsPerPage) {
+        this.setPage(marker, forward, itemsPerPage);
+    }
+
+    public MarkerPaged setPage(String marker, boolean forward, int itemsPerPage){
+        this.marker = marker;
+        this.forward = forward;
+        super.setItemsPerPage(itemsPerPage);
+        return this;
+    }
 }
