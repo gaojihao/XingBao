041a6ab8a1c9f02f77f5590b5a6338eede1c897b
lizhi <lizhi1026@126.com>
2019-09-29 21:55:26 +0800

    add swagger

--- src/main/java/com/lizhi/xingbao/controller/CategoryController.java
+++ src/main/java/com/lizhi/xingbao/controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.lizhi.xingbao.common.Result;
 import com.lizhi.xingbao.dto.CourseCategotyDto;
 import com.lizhi.xingbao.service.CategoryService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,30 +15,35 @@ public class CategoryController extends BaseController{
     @Autowired
     private CategoryService service;
 
+    @ApiOperation(value = "添加分类", notes = "添加分类", httpMethod = "POST")
     @PostMapping("add")
     public Result add(@ModelAttribute CourseCategotyDto dto){
         service.createCategory(dto);
         return Result.success(null);
     }
 
+    @ApiOperation(value = "删除分类", notes = "删除分类", httpMethod = "GET")
     @GetMapping("delete")
     public Result delete(@RequestParam(name = "id") Integer Id){
         service.deleteCategoty(Id);
         return Result.success(null);
     }
 
+    @ApiOperation(value = "编辑分类", notes = "编辑分类", httpMethod = "POST")
     @PostMapping("edit")
     public Result edit(@ModelAttribute CourseCategotyDto dto){
         service.editCategory(dto);
         return Result.success(null);
     }
 
+    @ApiOperation(value = "获取分类详情", notes = "获取分类详情", httpMethod = "GET")
     @GetMapping("detail")
     public Result detail(@RequestParam(name = "id") Integer Id){
 
         return Result.success(service.categoryDetail(Id));
     }
 
+    @ApiOperation(value = "获取分类列表", notes = "获取分类列表", httpMethod = "GET")
     @GetMapping("list")
     public Result list(){
 
