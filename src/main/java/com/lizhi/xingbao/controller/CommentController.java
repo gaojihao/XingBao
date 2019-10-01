1a3968e4ef391c0672acb4665a253d28e2e87096
lizhi <lizhi1026@126.com>
2019-10-01 16:00:02 +0800

    update comment service

--- src/main/java/com/lizhi/xingbao/controller/CommentController.java
+++ src/main/java/com/lizhi/xingbao/controller/CommentController.java
@@ -4,12 +4,13 @@ import com.lizhi.xingbao.common.Result;
 import com.lizhi.xingbao.dto.CommentDto;
 import com.lizhi.xingbao.request.CommentRequest;
 import com.lizhi.xingbao.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "评论")
 @RestController
 @RequestMapping("xingbao/api/v1/comment")
 public class CommentController extends BaseController{
@@ -17,20 +18,16 @@ public class CommentController extends BaseController{
     @Autowired
     private CommentService commentService;
 
-    @RequestMapping("add")
-    public Result add(@RequestBody CommentDto dto) {
+    @ApiOperation(value = "评论", notes = "评论", httpMethod = "POST")
+    @PostMapping("add")
+    public Result add(@ModelAttribute CommentDto dto) {
         commentService.createComment(dto);
         return Result.success(null);
     }
 
-    @RequestMapping("delete")
-    public Result delete(@RequestBody Integer Id) {
-        commentService.deleteDetail(Id);
-        return Result.success(null);
-    }
-
-    @RequestMapping("list")
-    public Result list(@RequestBody Integer courseId, @RequestBody Pageable pageable){
+    @ApiOperation(value = "获取评论列表", notes = "获取评论列表", httpMethod = "GET")
+    @GetMapping("list")
+    public Result list(Integer courseId, Pageable pageable){
 
         return Result.success(commentService.findAllBy(courseId,pageable));
     }
