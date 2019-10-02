556e0a740a751fc0e704885947ea317acd16f097
lizhi <lizhi1026@126.com>
2019-10-02 23:44:15 +0800

    update course controller

--- src/main/java/com/lizhi/xingbao/controller/CourseController.java
+++ src/main/java/com/lizhi/xingbao/controller/CourseController.java
@@ -7,9 +7,8 @@ import com.lizhi.xingbao.service.CourseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.print.Pageable;
 
 @Api(tags = "课程")
 @RestController
@@ -22,34 +21,35 @@ public class CourseController extends BaseController{
     @ApiOperation(value = "添加课程", notes = "添加课程", httpMethod = "POST")
     @PostMapping("add")
     public Result add(@ModelAttribute CourseDto dto) {
+        courseService.add(dto);
         return Result.success(null);
     }
 
     @ApiOperation(value = "删除课程", notes = "删除课程", httpMethod = "GET")
     @GetMapping("delete")
     public Result delete(@RequestParam(name = "courseId") Integer Id) {
-
+        courseService.deleteCourse(Id);
         return Result.success(null);
     }
 
     @ApiOperation(value = "编辑课程", notes = "编辑课程", httpMethod = "POST")
     @PostMapping("edit")
     public Result edit(@ModelAttribute CourseDto dto) {
-
+        courseService.editCourse(dto);
         return Result.success(null);
     }
 
     @ApiOperation(value = "获取课程详情", notes = "获取课程详情", httpMethod = "GET")
     @GetMapping("detail")
     public Result detail(@RequestParam(name = "courseId") Integer Id) {
-        return Result.success(null);
+        return Result.success(courseService.courseDetail(Id));
     }
 
     @ApiOperation(value = "获取课程列表", notes = "获取课程列表", httpMethod = "GET")
     @GetMapping("list")
     public Result list(CourseQueryCriteria criteria, Pageable pageable) {
 
-        return Result.success(null);
+        return Result.success(courseService.getCourseList(criteria,pageable));
     }
 
 
