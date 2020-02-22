ea63ceef444923121ac65712799286f6905a5561
lizhi <lizhi1026@126.com>
2020-02-22 21:17:10 +0800

    update sql

--- backend.sql
+++ backend.sql
@@ -126,4 +126,14 @@ CREATE TABLE IF NOT EXISTS `course` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2019 DEFAULT CHARSET=utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT='课程';
 
+CREATE TABLE IF NOT EXISTS `feedback` (
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '唯一id，自动生成',
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `content` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '内容',
+    `type` int(11) DEFAULT NULL COMMENT '反馈类型',
+    `create_user` bigint(20) unsigned DEFAULT NULL COMMENT '创建人',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3209 DEFAULT CHARSET=utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT='意见反馈';
+
 
